d9494be333045d6623b54ff3455035a0af9d476e
alishariq <57757461+alishariq@users.noreply.github.com>
2020-06-15 16:07:54 +0500

    Update doWhileRandomNumber.js

--- doWhileRandomNumber.js
+++ doWhileRandomNumber.js
@@ -17,3 +17,25 @@ do {
 } while (!correctGuess)
   document.write("<h1>You guessed the number!</h1>");
  document.write(`It took you ${guessCount} tries to guess the number ${randomNumber}`);
+
+
+
+
+
+//Shortcut to doing this by another:
+
+var randomNumber = getRandomNumber(10);
+var guess;
+var guessCount = 0;
+function getRandomNumber( upper ) {
+  var num = Math.floor(Math.random() * upper) + 1; 
+  return num;
+}
+
+do {
+  guess= prompt("I'm thinking of a number between 1 and 10...what is ist?");
+  guessCount +=1;
+  
+} while  (parseInt(guess)!==randomNumber)
+  document.write ("<h1> You guessed the correct number!</h1>");
+  document.write ("It took you "+guessCount+" tries to guess the number "+randomNumber);
